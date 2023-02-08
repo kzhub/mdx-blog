27086a13cf51c0357dd92fd2e0dafdce86b9ab9e
kzhub <kazukicoronek@gmail.com>
2023-02-09 02:05:42 +0900

    add chakra ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Link from 'next/link'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Document() {
 	return (
-		<Html lang="ja">
-			<Head />
-			<body>
-				<h1>
-					<Link href='/'>
-					Markdown Note
-					</Link>
-				</h1>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
+		<ChakraProvider>
+			<Html lang="ja">
+				<Head />
+				<body>
+					<h1>
+						<Link href='/'>
+							Markdown Note
+						</Link>
+					</h1>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		</ChakraProvider>
 	)
 }
