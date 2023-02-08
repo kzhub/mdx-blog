6ae792f161c1f0e5e52e328f64690220a6eb6a5d
kzhub <kazukicoronek@gmail.com>
2023-02-09 02:05:32 +0900

    add chakra ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
 	return (
 		<Html lang="ja">
 			<Head />
 			<body>
-				<h1>Markdown Note</h1>
+				<h1>
+					<Link href='/'>
+					Markdown Note
+					</Link>
+				</h1>
 				<Main />
 				<NextScript />
 			</body>
